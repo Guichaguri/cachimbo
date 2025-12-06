2de82449baf48a2d31ddbe1add9d233ff18e7e64
Guilherme Chaguri <guichaguri@gmail.com>
2025-12-06 06:28:39 -0300

    feat(kv): added option to set the TTL for the edge caching

--- src/remote/workers-kv/index.ts
+++ src/remote/workers-kv/index.ts
@@ -7,21 +7,32 @@ export interface WorkersKVCacheOptions extends BaseCacheOptions {
    * The KV namespace (`env.NAMESPACE`)
    */
   kv: KVNamespace;
+
+  /**
+   * The amount of time in seconds that a KV result is cached in the global network location it is accessed from.
+   *
+   * Increasing this value can improve read performance at the cost of data being stale.
+   *
+   * @see https://developers.cloudflare.com/kv/api/read-key-value-pairs/#cachettl-parameter
+   */
+  edgeCacheTTL?: number;
 }
 
 /**
- * A CloudFlare Workers KV cache implementation.
+ * A Cloudflare Workers KV cache implementation.
  */
 export class WorkersKVCache extends BaseCache {
   protected readonly kv: KVNamespace;
+  protected edgeCacheTTL?: number;
 
   constructor(options: WorkersKVCacheOptions) {
     super(options);
     this.kv = options.kv;
+    this.edgeCacheTTL = options.edgeCacheTTL;
   }
 
   get<T>(key: string): Promise<T | null> {
-    return this.kv.get<T>(key, { type: 'json' });
+    return this.kv.get<T>(key, { type: 'json', cacheTtl: this.edgeCacheTTL });
   }
 
   set<T>(key: string, value: T, options: SetCacheOptions = {}): Promise<void> {
